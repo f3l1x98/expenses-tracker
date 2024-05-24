cc3571338dc7745f6faadd8307a221aecab92b36
felix <felix.mueller0998@gmx.de>
2024-05-24 12:29:55 +0200

    Fixed User import error

--- expenses-backend/src/app/auth/local.strategy.ts
+++ expenses-backend/src/app/auth/local.strategy.ts
@@ -2,7 +2,8 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy } from 'passport-local';
 
-import { AuthService, User } from './auth.service';
+import { AuthService } from './auth.service';
+import { IUser } from '../users/entities/user';
 
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
@@ -10,7 +11,7 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
     super();
   }
 
-  async validate(username: string, password: string): Promise<User> {
+  async validate(username: string, password: string): Promise<IUser> {
     const user = await this.authService.validateUser(username, password);
     if (!user) {
       throw new UnauthorizedException();
