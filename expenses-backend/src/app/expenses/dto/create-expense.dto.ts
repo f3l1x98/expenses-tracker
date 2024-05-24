058e023a819cc91c1835df8bbd16a19215abcb82
felix <felix.mueller0998@gmx.de>
2024-05-24 12:44:37 +0200

    Made dto non abstract for nestjs PartialType

--- expenses-backend/src/app/expenses/dto/create-expense.dto.ts
+++ expenses-backend/src/app/expenses/dto/create-expense.dto.ts
@@ -1,7 +1,7 @@
 import { IsNumber, IsPositive } from 'class-validator';
 import { ExpenseCategory } from '../entities/expense-category';
 
-export abstract class CreateExpenseDto {
+export class CreateExpenseDto {
   // TODO has IsCurrency which expects string
   @IsNumber({ maxDecimalPlaces: 2, allowInfinity: false, allowNaN: false })
   @IsPositive()
