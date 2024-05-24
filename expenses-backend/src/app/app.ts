adbbfc96a67a235f58d37e64827014991ba3efca
felix <felix.mueller0998@gmx.de>
2024-05-24 12:25:11 +0200

    Removed winston

--- expenses-backend/src/app/app.ts
+++ expenses-backend/src/app/app.ts
@@ -5,16 +5,9 @@ import {
 } from '@nestjs/common';
 import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { createLogger, format, transports } from 'winston';
-import { WinstonModule } from 'nest-winston';
 
 export async function initApp(): Promise<INestApplication> {
-  const loggerInstance = createWinstonLogger();
-  const app = await NestFactory.create(AppModule, {
-    logger: WinstonModule.createLogger({
-      instance: loggerInstance,
-    }),
-  });
+  const app = await NestFactory.create(AppModule);
 
   app.enableCors();
 
@@ -26,22 +19,4 @@ export async function initApp(): Promise<INestApplication> {
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
 
   return app;
-}
-
-function createWinstonLogger() {
-  return createLogger({
-    level: 'info',
-    format: format.combine(
-      format.timestamp(),
-      format.errors(),
-      format.colorize(),
-    ),
-    exitOnError: false,
-    transports: [
-      new transports.Console({
-        handleExceptions: true,
-        handleRejections: true,
-      }),
-    ],
-  });
 }
