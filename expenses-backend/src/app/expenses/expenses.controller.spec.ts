16327aca8574e4a5a9825d8e990917eeda937a0a
felix <felix.mueller0998@gmx.de>
2024-05-26 17:42:55 +0200

    Experimenting with controller tests

---
+++ expenses-backend/src/app/expenses/expenses.controller.spec.ts
@@ -0,0 +1,37 @@
+import { Test } from '@nestjs/testing';
+import { ExpensesController } from './expenses.controller';
+import { ExpensesService } from './expenses.service';
+import { ExpenseEntity } from './entities/expense.entity';
+import { Request } from 'express';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+describe('ExpensesController', () => {
+  let expensesController: ExpensesController;
+  let expensesService: ExpensesService;
+
+  beforeEach(async () => {
+    const moduleRef = await Test.createTestingModule({
+      // TODO missing TypeOrm
+      imports: [TypeOrmModule.forFeature([ExpenseEntity])],
+      controllers: [ExpensesController],
+      providers: [ExpensesService],
+    }).compile();
+
+    expensesService = moduleRef.get<ExpensesService>(ExpensesService);
+    expensesController = moduleRef.get<ExpensesController>(ExpensesController);
+  });
+
+  describe('findOwn', () => {
+    it('should return an array of expenses', async () => {
+      const expense = new ExpenseEntity();
+      const result = [expense];
+      jest.spyOn(expensesService, 'findAllForUser').mockResolvedValue(result);
+
+      expect(
+        await expensesController.findOwn({
+          user: { id: 'TestId' },
+        } as unknown as Request),
+      ).toBe(result);
+    });
+  });
+});
