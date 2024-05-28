d0d6b608790165f5c6a71bc2dd3f4813a0f9414a
felix <felix.mueller0998@gmx.de>
2024-05-29 00:40:07 +0200

    Import StoreModule and EffectsModule

--- expenses-web/src/app/core/auth/auth.module.ts
+++ expenses-web/src/app/core/auth/auth.module.ts
@@ -1,9 +1,18 @@
 import { NgModule } from '@angular/core';
 import { LoginComponent } from './components/login/login.component';
 import { AuthRoutingModule } from './auth-routing.module';
+import { StoreModule } from '@ngrx/store';
+import { authFeature } from './store/auth.feature';
+import { EffectsModule } from '@ngrx/effects';
+import { AuthEffects } from './store/auth.effects';
 
 @NgModule({
-  imports: [AuthRoutingModule, LoginComponent],
+  imports: [
+    AuthRoutingModule,
+    LoginComponent,
+    StoreModule.forFeature(authFeature),
+    EffectsModule.forFeature([AuthEffects]),
+  ],
   exports: [],
   declarations: [],
   providers: [],
