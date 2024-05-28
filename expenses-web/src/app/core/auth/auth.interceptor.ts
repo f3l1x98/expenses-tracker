f5496154e3c40a55fe1ce3b083e9bcdc21ccbab5
felix <felix.mueller0998@gmx.de>
2024-05-28 13:14:06 +0200

    Started with interceptor for injecting access token to requests

---
+++ expenses-web/src/app/core/auth/auth.interceptor.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpInterceptor,
+  HttpEvent,
+  HttpHandler,
+  HttpRequest,
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+  constructor(private auth: TODO) {}
+
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    const authToken = this.auth.accessToken;
+
+    const authReq = req.clone({
+      headers: req.headers.set('Authorization', `Bearer ${authToken}`),
+    });
+
+    return next.handle(authReq);
+  }
+}
