bd74d49132a81776cfd11125dbae1d54406d6a3f
felix <felix.mueller0998@gmx.de>
2024-05-30 01:26:35 +0200

    Add authGuard impl that checks the AuthService.status

--- expenses-web/src/app/core/auth/auth.guard.ts
+++ expenses-web/src/app/core/auth/auth.guard.ts
@@ -2,12 +2,26 @@ import { inject } from '@angular/core';
 import {
   ActivatedRouteSnapshot,
   CanActivateFn,
+  Router,
   RouterStateSnapshot,
 } from '@angular/router';
+import { map } from 'rxjs';
+import { AuthService } from './auth.service';
 
 export const AuthGuard: CanActivateFn = (
   route: ActivatedRouteSnapshot,
   state: RouterStateSnapshot
 ) => {
-  return false;
+  const authService = inject(AuthService);
+  const router = inject(Router);
+  return authService.status$.pipe(
+    map((status) => {
+      if (status.value == 'success') {
+        return true;
+      } else {
+        router.navigate(['/login']);
+        return false;
+      }
+    })
+  );
 };
