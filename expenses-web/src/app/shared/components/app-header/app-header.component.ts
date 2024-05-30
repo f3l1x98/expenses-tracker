20cc7b87eeab7268240e9166ac87e413399fdf05
felix <felix.mueller0998@gmx.de>
2024-05-30 22:28:35 +0200

    Extract app header into separate component for reuse

---
+++ expenses-web/src/app/shared/components/app-header/app-header.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: 'app-header.component.html',
+  standalone: true,
+})
+export class AppHeaderComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
--- expenses-web/src/app/shell/sidebar/sidebar.component.ts
+++ expenses-web/src/app/shell/sidebar/sidebar.component.ts
@@ -2,19 +2,25 @@ import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { AuthService } from '../../core/auth/auth.service';
+import { NotificationService } from '../notification/notification.service';
+import { AppHeaderComponent } from '../../shared/components/app-header/app-header.component';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: 'sidebar.component.html',
   standalone: true,
-  imports: [RouterModule, ButtonModule],
+  imports: [RouterModule, ButtonModule, AppHeaderComponent],
 })
 export class SidebarComponent implements OnInit {
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private notificationService: NotificationService
+  ) {}
 
   ngOnInit() {}
 
   logout() {
-    this.authService.logout();
+    this.notificationService.success('test');
+    //this.authService.logout();
   }
 }
