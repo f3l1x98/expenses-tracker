31cdeaa3b5974b131fc85c4adf568c0fcdfee607
felix <felix.mueller0998@gmx.de>
2024-05-31 16:28:02 +0200

    Add simple formatDate pipe

---
+++ expenses-web/src/app/shared/shared.nodule.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { FormatDatePipe } from './pipes/format-date.pipe';
+
+@NgModule({
+  imports: [],
+  exports: [FormatDatePipe],
+  declarations: [FormatDatePipe],
+  providers: [],
+})
+export class SharedModule {}
