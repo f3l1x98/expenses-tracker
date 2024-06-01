6ddcea4338017ddcedd70b5e5a4f3a7f95140609
felix <felix.mueller0998@gmx.de>
2024-06-01 13:36:14 +0200

    Experiment with ChartJs and date range filter

--- expenses-web/src/app/features/home/components/home.component.ts
+++ expenses-web/src/app/features/home/components/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartData, ChartOptions } from 'chart.js';
+import { Observable, map, of } from 'rxjs';
+import { ExpensesPerCategory } from '../store/interfaces/expenses-per-category.interface';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +9,55 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  expensesPerCategoryData$!: Observable<ChartData>;
+  expensesPerCategoryOptions!: ChartOptions<'pie'>;
+
+  dateRangeFilter: Date[] | undefined;
+
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    const documentStyle = getComputedStyle(document.documentElement);
+    const textColor = documentStyle.getPropertyValue('--text-color');
+
+    this.expensesPerCategoryData$ = of([
+      {
+        category: 'test1',
+        amount: 1,
+        colorName: '--blue-500',
+      },
+      {
+        category: 'test2',
+        amount: 10,
+        colorName: '--yellow-500',
+      },
+      {
+        category: 'test3',
+        amount: 3,
+        colorName: '--green-500',
+      },
+    ] as ExpensesPerCategory[]).pipe(
+      map((apiData) => ({
+        labels: apiData.map((entries) => entries.category),
+        datasets: [
+          {
+            data: apiData.map((entries) => entries.amount),
+            backgroundColor: apiData.map((entries) =>
+              documentStyle.getPropertyValue(entries.colorName)
+            ),
+          },
+        ],
+      }))
+    );
+    this.expensesPerCategoryOptions = {
+      plugins: {
+        legend: {
+          labels: {
+            usePointStyle: true,
+            color: textColor,
+          },
+        },
+      },
+    };
+  }
 }
--- expenses-web/src/app/features/home/home.module.ts
+++ expenses-web/src/app/features/home/home.module.ts
@@ -2,9 +2,23 @@ import { NgModule } from '@angular/core';
 
 import { HomeComponent } from './components/home.component';
 import { HomeRoutingModule } from './home-routing.module';
+import { ChartModule } from 'primeng/chart';
+import { CardModule } from 'primeng/card';
+import { CommonModule } from '@angular/common';
+import { AppContentWrapperComponent } from '../../shared/components/app-content-wrapper/app-content-wrapper.component';
+import { CalendarModule } from 'primeng/calendar';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  imports: [HomeRoutingModule],
+  imports: [
+    HomeRoutingModule,
+    ChartModule,
+    CardModule,
+    CommonModule,
+    CalendarModule,
+    AppContentWrapperComponent,
+    FormsModule,
+  ],
   exports: [],
   declarations: [HomeComponent],
   providers: [],
