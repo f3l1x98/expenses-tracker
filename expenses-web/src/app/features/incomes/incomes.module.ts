d7a3a7a86d0d7ebbbbf23092b436ff73b553fb3c
felix <felix.mueller0998@gmx.de>
2024-06-02 17:35:58 +0200

    Activate IncomesFeature and Effect

--- expenses-web/src/app/features/incomes/incomes.module.ts
+++ expenses-web/src/app/features/incomes/incomes.module.ts
@@ -3,11 +3,21 @@ import { NgModule } from '@angular/core';
 import { IncomesComponent } from './components/incomes.component';
 import { IncomesRoutingModule } from './incomes-routing.module';
 import { AppContentWrapperComponent } from '../../shared/components/app-content-wrapper/app-content-wrapper.component';
+import { IncomesService } from './incomes.service';
+import { incomesFeature } from './store/features/incomes.feature';
+import { IncomesEffect } from './store/effects/incomes.effects';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
 
 @NgModule({
-  imports: [IncomesRoutingModule, AppContentWrapperComponent],
+  imports: [
+    IncomesRoutingModule,
+    AppContentWrapperComponent,
+    StoreModule.forFeature(incomesFeature),
+    EffectsModule.forFeature([IncomesEffect]),
+  ],
   exports: [],
   declarations: [IncomesComponent],
-  providers: [],
+  providers: [IncomesService],
 })
 export class IncomesModule {}
