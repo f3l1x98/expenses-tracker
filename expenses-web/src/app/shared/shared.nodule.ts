cef650ea24618ff1f2b046abbd09b4dcba6bb481
felix <felix.mueller0998@gmx.de>
2024-06-02 23:28:35 +0200

    Add simple formatCurrency pipe for formatting currencies

--- expenses-web/src/app/shared/shared.nodule.ts
+++ expenses-web/src/app/shared/shared.nodule.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { FormatDatePipe } from './pipes/format-date.pipe';
+import { FormatCurrencyPipe } from './pipes/format-currency.pipe';
 
 @NgModule({
   imports: [],
-  exports: [FormatDatePipe],
-  declarations: [FormatDatePipe],
+  exports: [FormatDatePipe, FormatCurrencyPipe],
+  declarations: [FormatDatePipe, FormatCurrencyPipe],
   providers: [],
 })
 export class SharedModule {}
