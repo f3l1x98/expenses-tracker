bd839e00941b786a2bb572a55160386e6cafca4a
felix <felix.mueller0998@gmx.de>
2024-06-05 00:49:46 +0200

    Add TypeOrmModule for UserEntity to imports

--- expenses-backend/src/app/dashboard/dashboard.module.ts
+++ expenses-backend/src/app/dashboard/dashboard.module.ts
@@ -4,11 +4,13 @@ import { ExpenseEntity } from '../expenses/entities/expense.entity';
 import { IncomeEntity } from '../incomes/entities/income.entity';
 import { DashboardController } from './dashboard.controller';
 import { DashboardService } from './dashboard.service';
+import { UserEntity } from '../users/entities/user.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([ExpenseEntity]),
     TypeOrmModule.forFeature([IncomeEntity]),
+    TypeOrmModule.forFeature([UserEntity]),
   ],
   controllers: [DashboardController],
   providers: [DashboardService],
