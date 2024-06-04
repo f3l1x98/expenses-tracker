996205b9751400bc269b0c595bc755f9f1fb9e1c
felix <felix.mueller0998@gmx.de>
2024-06-05 00:51:03 +0200

    Add category to colors for Dashboards API (atm default all same green color)

--- expenses-backend/src/app/expenses/entities/expense-category.ts
+++ expenses-backend/src/app/expenses/entities/expense-category.ts
@@ -9,3 +9,20 @@ export enum ExpenseCategory {
   MISC = 'misc',
   // TODO
 }
+
+const categoryColors: { [key in ExpenseCategory]: string } = {
+  [ExpenseCategory.CAR]: '--green-500',
+  [ExpenseCategory.CLOTHING]: '--green-500',
+  [ExpenseCategory.ENTERTAINMENT]: '--green-500',
+  [ExpenseCategory.GROCERY]: '--green-500',
+  [ExpenseCategory.HEALTHCARE]: '--green-500',
+  [ExpenseCategory.INVOICE]: '--green-500',
+  [ExpenseCategory.MISC]: '--green-500',
+  [ExpenseCategory.VACATION]: '--green-500',
+};
+
+export function getExpenseCategoryColor(
+  expenseCategory: ExpenseCategory,
+): string {
+  return categoryColors[expenseCategory];
+}
