95d1525639f153c120101fc1188668596743fd3e
felix <felix.mueller0998@gmx.de>
2024-06-05 00:51:40 +0200

    Add first implementation of queries

--- expenses-backend/src/app/dashboard/dashboard.service.ts
+++ expenses-backend/src/app/dashboard/dashboard.service.ts
@@ -4,9 +4,30 @@ import { Repository } from 'typeorm';
 import { ExpenseEntity } from '../expenses/entities/expense.entity';
 import { DateRangeDto } from '../shared/date-range.dto';
 import { CurrentMonthData } from './dto/current-month-data.dto';
-import { ExpensesPerCategory } from './dto/expenses-per-category.dto';
-import { ExpensesPerMonth } from './dto/expenses-per-month.dto';
+import {
+  ExpensesPerCategory,
+  TotalExpenseOfCategory,
+} from './dto/expenses-per-category.dto';
+import {
+  ExpensesPerMonth,
+  TotalExpenseOfMonth,
+} from './dto/expenses-per-month.dto';
 import { IncomeEntity } from '../incomes/entities/income.entity';
+import {
+  ExpenseCategory,
+  getExpenseCategoryColor,
+} from '../expenses/entities/expense-category';
+import { UserEntity } from '../users/entities/user.entity';
+
+interface ExpensesPerCategoryQueryResponse {
+  category: ExpenseCategory;
+  totalAmount: number;
+}
+
+interface ExpensesPerMonthQueryResponse {
+  monthYear: string;
+  totalAmount: number;
+}
 
 @Injectable()
 export class DashboardService {
@@ -15,29 +36,98 @@ export class DashboardService {
     private expensesRepository: Repository<ExpenseEntity>,
     @InjectRepository(IncomeEntity)
     private incomesRepository: Repository<IncomeEntity>,
+    @InjectRepository(UserEntity)
+    private usersRepository: Repository<UserEntity>,
   ) {}
 
-  getCurrentMonthData(userId: string): CurrentMonthData {
+  async getCurrentMonthData(userId: string): Promise<CurrentMonthData> {
+    const userResult = await this.usersRepository.findOne({
+      where: { id: userId },
+    });
+
+    const now = new Date();
+    const startOfMonth = new Date(now.getFullYear(), now.getMonth(), 1);
+    const expensesResult = await this.expensesRepository
+      .createQueryBuilder('expense')
+      .addSelect('SUM(expense.amount)', 'totalAmount')
+      .innerJoin('expense.user', 'user')
+      .where('user.id = :userId', { userId })
+      .andWhere('expense.createdAt >= :startOfMonth', { startOfMonth })
+      .getRawOne<number>();
+    const incomesResult = await this.incomesRepository
+      .createQueryBuilder('income')
+      .addSelect('SUM(income.amount)', 'totalAmount')
+      .innerJoin('income.user', 'user')
+      .where('user.id = :userId', { userId })
+      .andWhere('income.createdAt >= :startOfMonth', { startOfMonth })
+      .getRawOne<number>();
+
     const currentMonthData = new CurrentMonthData();
-    // TODO retrieve data from repositories
+    currentMonthData.currency = userResult?.settings.currency ?? '';
+    currentMonthData.totalExpense = expensesResult ?? 0;
+    currentMonthData.totalIncome = incomesResult ?? 0;
+    currentMonthData.balance =
+      currentMonthData.totalIncome - currentMonthData.totalExpense;
     return currentMonthData;
   }
 
-  getExpensesPerCategory(
+  async getExpensesPerCategory(
     userId: string,
     dateRange: DateRangeDto,
-  ): ExpensesPerCategory {
+  ): Promise<ExpensesPerCategory> {
+    const result = await this.expensesRepository
+      .createQueryBuilder('expense')
+      .select('expense.category', 'category')
+      .addSelect('SUM(expense.amount)', 'totalAmount')
+      .innerJoin('expense.user', 'user')
+      .where('user.id = :userId', { userId })
+      .andWhere('expense.createdAt BETWEEN :startDate AND :endDate', dateRange)
+      .groupBy('expense.category')
+      .getRawMany<ExpensesPerCategoryQueryResponse>();
+
+    const userResult = await this.usersRepository.findOne({
+      where: { id: userId },
+    });
+    console.dir(result, { depth: null });
+
     const expensesPerCategory = new ExpensesPerCategory();
-    // TODO retrieve data from repositories
+    expensesPerCategory.currency = userResult?.settings.currency ?? '';
+    expensesPerCategory.data = result.map(
+      (item) =>
+        new TotalExpenseOfCategory(
+          item.category,
+          item.totalAmount,
+          getExpenseCategoryColor(item.category),
+        ),
+    );
+    console.dir(expensesPerCategory.data, { depth: null });
     return expensesPerCategory;
   }
 
-  getExpensesPerMonth(
+  async getExpensesPerMonth(
     userId: string,
     dateRange: DateRangeDto,
-  ): ExpensesPerMonth {
+  ): Promise<ExpensesPerMonth> {
+    const result = await this.expensesRepository
+      .createQueryBuilder('expense')
+      .select("TO_CHAR(expense.createdAt, 'YYYY-MM')", 'monthYear')
+      .addSelect('SUM(expense.amount)', 'totalAmount')
+      .innerJoin('expense.user', 'user')
+      .where('user.id = :userId', { userId })
+      .andWhere('expense.createdAt BETWEEN :startDate AND :endDate', dateRange)
+      .groupBy('"monthYear"')
+      .orderBy('"monthYear"')
+      .getRawMany<ExpensesPerMonthQueryResponse>();
+
+    const userResult = await this.usersRepository.findOne({
+      where: { id: userId },
+    });
+
     const expensesPerMonth = new ExpensesPerMonth();
-    // TODO retrieve data from repositories
+    expensesPerMonth.currency = userResult?.settings.currency ?? '';
+    expensesPerMonth.data = result.map(
+      (item) => new TotalExpenseOfMonth(item.monthYear, item.totalAmount),
+    );
     return expensesPerMonth;
   }
 }
