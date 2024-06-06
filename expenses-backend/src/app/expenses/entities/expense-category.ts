5beaca0d45ab8cc8f64cef52f16576d4e8d5a0da
felix <felix.mueller0998@gmx.de>
2024-06-06 17:06:59 +0200

    Change category colors

--- expenses-backend/src/app/expenses/entities/expense-category.ts
+++ expenses-backend/src/app/expenses/entities/expense-category.ts
@@ -11,14 +11,14 @@ export enum ExpenseCategory {
 }
 
 const categoryColors: { [key in ExpenseCategory]: string } = {
-  [ExpenseCategory.CAR]: '--green-500',
-  [ExpenseCategory.CLOTHING]: '--green-500',
-  [ExpenseCategory.ENTERTAINMENT]: '--green-500',
+  [ExpenseCategory.CAR]: '--blue-500',
+  [ExpenseCategory.CLOTHING]: '--yellow-500',
+  [ExpenseCategory.ENTERTAINMENT]: '--purple-500',
   [ExpenseCategory.GROCERY]: '--green-500',
-  [ExpenseCategory.HEALTHCARE]: '--green-500',
-  [ExpenseCategory.INVOICE]: '--green-500',
-  [ExpenseCategory.MISC]: '--green-500',
-  [ExpenseCategory.VACATION]: '--green-500',
+  [ExpenseCategory.HEALTHCARE]: '--red-500',
+  [ExpenseCategory.INVOICE]: '--orange-500',
+  [ExpenseCategory.MISC]: '--gray-500',
+  [ExpenseCategory.VACATION]: '--indigo-500',
 };
 
 export function getExpenseCategoryColor(
