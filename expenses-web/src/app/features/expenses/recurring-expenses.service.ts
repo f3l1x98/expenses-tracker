6bbba284dd5e25bb93ee422823ec332170d513ce
felix <felix.mueller0998@gmx.de>
2024-06-07 22:32:29 +0200

    Add create logic to recurring expense store and refactor existing store to allow create status

--- expenses-web/src/app/features/expenses/recurring-expenses.service.ts
+++ expenses-web/src/app/features/expenses/recurring-expenses.service.ts
@@ -1,14 +1,20 @@
 import { Injectable } from '@angular/core';
 import { RecurringExpensesStoreService } from './store/services/recurring-expenses-store.service';
+import { CreateRecurringExpenseRequest } from './api/interfaces/requests/create-recurring-expense-request.interface';
 
 @Injectable()
 export class RecurringExpensesService {
-  status$ = this.store.status$;
+  loadStatus$ = this.store.loadStatus$;
   recurringExpenses$ = this.store.recurringExpenses$;
+  createStatus$ = this.store.createStatus$;
 
   constructor(private store: RecurringExpensesStoreService) {}
 
   load() {
     this.store.load();
+  }
+
+  create(request: CreateRecurringExpenseRequest) {
+    this.store.create(request);
   }
 }
