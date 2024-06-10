a7bc3e92f7868d7743bacd3c0dd5307b71b38412
felix <felix.mueller0998@gmx.de>
2024-06-10 08:31:41 +0200

    Separate auth user because auth does not care about user settings etc

--- expenses-backend/src/app/auth/auth.service.ts
+++ expenses-backend/src/app/auth/auth.service.ts
@@ -5,7 +5,11 @@ https://docs.nestjs.com/providers#services
 import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { UsersService } from '../users/users.service';
-import { IUser } from '../users/entities/user';
+
+export interface User {
+  id: string;
+  username: string;
+}
 
 @Injectable()
 export class AuthService {
@@ -17,18 +21,18 @@ export class AuthService {
   async validateUser(
     name: string,
     password: string,
-  ): Promise<IUser | undefined> {
+  ): Promise<User | undefined> {
     const user = await this.usersService.findOne(name);
     const isValid = await this.usersService.validate(name, password);
 
     if (user && isValid) {
-      return { id: user.id, username: user.username, settings: user.settings };
+      return { id: user.id, username: user.username };
     }
 
     return undefined;
   }
 
-  async generateJwt(user: IUser, expiresIn?: number | string): Promise<string> {
+  async generateJwt(user: User, expiresIn?: number | string): Promise<string> {
     const payload = { username: user.username, sub: user.id };
 
     if (expiresIn !== undefined) {
