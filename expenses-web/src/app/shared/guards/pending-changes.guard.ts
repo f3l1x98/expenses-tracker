70f449688d347875a4a1de40efec2e4a58689fc0
felix <felix.mueller0998@gmx.de>
2024-06-13 00:00:01 +0200

    Add pending changes guard

---
+++ expenses-web/src/app/shared/guards/pending-changes.guard.ts
@@ -0,0 +1,19 @@
+import {
+  ActivatedRouteSnapshot,
+  CanDeactivateFn,
+  RouterStateSnapshot,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+
+export interface ComponentCanDeactivate {
+  canDeactivate: () => boolean | Observable<boolean>;
+}
+
+export const PendingChangesGuard: CanDeactivateFn<ComponentCanDeactivate> = (
+  component: ComponentCanDeactivate,
+  currentRoute: ActivatedRouteSnapshot,
+  currentState: RouterStateSnapshot,
+  nextState: RouterStateSnapshot
+) => {
+  return component.canDeactivate ? component.canDeactivate() : true;
+};
