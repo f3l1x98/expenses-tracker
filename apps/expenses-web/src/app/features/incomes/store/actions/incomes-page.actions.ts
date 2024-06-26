ea336570542853b8047dc232ff17f356c91ef0ac
felix <felix.mueller0998@gmx.de>
2024-06-26 13:39:59 +0200

    Add income filter to income state

--- apps/expenses-web/src/app/features/incomes/store/actions/incomes-page.actions.ts
+++ apps/expenses-web/src/app/features/incomes/store/actions/incomes-page.actions.ts
@@ -1,5 +1,5 @@
 import { createAction, props } from '@ngrx/store';
-import { ICreateIncomeDto } from 'expenses-shared';
+import { ICreateIncomeDto, IIncomeFilterDto } from 'expenses-shared';
 
 export const createRequest = createAction(
   '[Income Page Actions] create request',
@@ -8,4 +8,8 @@ export const createRequest = createAction(
 export const deleteRequest = createAction(
   '[Income Page Actions] delete request',
   props<{ id: string }>(),
+);
+export const updateFilter = createAction(
+  '[Income Page Actions] update filter',
+  props<{ filter: IIncomeFilterDto }>(),
 );
--- apps/expenses-web/src/app/features/incomes/store/features/incomes.feature.ts
+++ apps/expenses-web/src/app/features/incomes/store/features/incomes.feature.ts
@@ -1,10 +1,17 @@
 import { createFeature, createReducer, on } from '@ngrx/store';
 import { IncomesState } from '../interfaces/incomes.state';
 import * as ApiActions from '../actions/incomes-api.actions';
+import * as PageActions from '../actions/incomes-page.actions';
 import { StoreStateStatus } from '../../../../shared/interfaces/store-state-status.interface';
 
 export const initialState: IncomesState = {
   incomes: [],
+  filter: {
+    description: undefined,
+    category: undefined,
+    startDate: undefined,
+    endDate: undefined,
+  },
   loadStatus: {
     error: undefined,
     status: 'initial',
@@ -36,6 +43,8 @@ export const incomesFeature = createFeature({
       ...state,
       loadStatus: { status: 'error', error } as StoreStateStatus,
     })),
+
+    on(PageActions.updateFilter, (state, { filter }) => ({ ...state, filter })),
 
     on(ApiActions.createStart, (state) => ({
       ...state,
