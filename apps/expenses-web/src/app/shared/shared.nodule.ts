1869ce1569681c49bb227385b9b2b3b88fe5dfca
felix <felix.mueller0998@gmx.de>
2024-06-29 12:51:08 +0200

    Add NextCronExecutionPipe to SharedModule

--- apps/expenses-web/src/app/shared/shared.nodule.ts
+++ apps/expenses-web/src/app/shared/shared.nodule.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { FormatDatePipe } from './pipes/format-date.pipe';
 import { FormatCurrencyPipe } from './pipes/format-currency.pipe';
+import { NextCronExecutionPipe } from './pipes/next-cron-execution.pipe';
 
 @NgModule({
   imports: [],
-  exports: [FormatDatePipe, FormatCurrencyPipe],
-  declarations: [FormatDatePipe, FormatCurrencyPipe],
+  exports: [FormatDatePipe, FormatCurrencyPipe, NextCronExecutionPipe],
+  declarations: [FormatDatePipe, FormatCurrencyPipe, NextCronExecutionPipe],
   providers: [],
 })
 export class SharedModule {}
