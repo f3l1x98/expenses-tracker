83adf2fb7060b30acef83bc6fca8ff861a2d7d27
felix <felix.mueller0998@gmx.de>
2024-06-29 13:07:57 +0200

    Add version 1.0 of recurring-expense-list

--- apps/expenses-web/src/app/features/expenses/components/recurring-expenses-list/recurring-expenses-list.component.ts
+++ apps/expenses-web/src/app/features/expenses/components/recurring-expenses-list/recurring-expenses-list.component.ts
@@ -1,13 +1,18 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Subject, takeUntil } from 'rxjs';
+import { BehaviorSubject, Subject, takeUntil } from 'rxjs';
 import { SpinnerService } from '../../../../shell/spinner/spinner.service';
 import { RecurringExpensesService } from '../../recurring-expenses.service';
+import { ConfirmationService, MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-recurring-expenses-list',
   templateUrl: 'recurring-expenses-list.component.html',
+  styleUrls: ['./recurring-expenses-list.component.scss'],
 })
 export class RecurringExpensesListComponent implements OnInit, OnDestroy {
+  actionMenuItems$: BehaviorSubject<MenuItem[]> = new BehaviorSubject(
+    [] as MenuItem[],
+  );
   recurringExpenses$ = this.service.recurringExpenses$;
 
   private destory$ = new Subject<void>();
@@ -15,6 +20,7 @@ export class RecurringExpensesListComponent implements OnInit, OnDestroy {
   constructor(
     private service: RecurringExpensesService,
     private spinnerService: SpinnerService,
+    private confirmationService: ConfirmationService,
   ) {}
 
   ngOnInit() {
@@ -29,6 +35,39 @@ export class RecurringExpensesListComponent implements OnInit, OnDestroy {
   ngOnDestroy(): void {
     this.destory$.next();
     this.destory$.complete();
+  }
+
+  // Workaround as described in https://github.com/primefaces/primeng/issues/13934#issuecomment-1887208083
+  // due to issue still not resolved
+  onMenuShow(recurringExpenseId: string) {
+    this.actionMenuItems$.next([
+      {
+        label: 'Edit',
+        icon: 'pi pi-pencil',
+        command() {
+          console.log('TODO EDIT');
+        },
+      },
+      {
+        label: 'Delete',
+        icon: 'pi pi-trash',
+        command: () => {
+          this.confirmationService.confirm({
+            message: 'Do you want to delete this recurring expense?',
+            header: 'Delete Confirmation',
+            icon: 'pi pi-info-circle',
+            acceptButtonStyleClass: 'p-button-danger p-button-text',
+            rejectButtonStyleClass: 'p-button-text p-button-text',
+            acceptIcon: 'none',
+            rejectIcon: 'none',
+
+            accept: () => {
+              this.service.delete(recurringExpenseId);
+            },
+          });
+        },
+      },
+    ]);
   }
 
   load() {
