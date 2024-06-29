8006e5e796346fc73effffc08e86b5a192e4e7cd
felix <felix.mueller0998@gmx.de>
2024-06-29 13:55:52 +0200

    Add directive that changes color to red if element content is negative currency

---
+++ apps/expenses-web/src/app/shared/directives/highlight-negative-currency.directive.ts
@@ -0,0 +1,29 @@
+import {
+  AfterViewInit,
+  Directive,
+  ElementRef,
+  Input,
+  Renderer2,
+} from '@angular/core';
+
+@Directive({ selector: '[appHighlightNegativeCurrency]' })
+export class HighlightNegativeCurrencyDirective implements AfterViewInit {
+  constructor(
+    private el: ElementRef,
+    private renderer: Renderer2,
+  ) {}
+
+  ngAfterViewInit(): void {
+    const text = this.el.nativeElement.textContent.trim();
+    const currencyValue = this.parseCurrency(text);
+
+    if (currencyValue < 0) {
+      this.renderer.setStyle(this.el.nativeElement, 'color', 'red');
+    }
+  }
+
+  private parseCurrency(value: string): number {
+    const numberString = value.replace(/[^0-9.-]/g, '');
+    return parseFloat(numberString);
+  }
+}
