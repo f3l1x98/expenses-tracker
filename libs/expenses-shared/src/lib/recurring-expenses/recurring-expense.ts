14743e80d3701459b2534bae680e87bb548ea7d1
felix <felix.mueller0998@gmx.de>
2024-07-24 16:27:27 +0200

    Move next execution into backend and remove cron from frontend

--- libs/expenses-shared/src/lib/recurring-expenses/recurring-expense.ts
+++ libs/expenses-shared/src/lib/recurring-expenses/recurring-expense.ts
@@ -2,7 +2,7 @@ import { IExpense } from '../expenses';
 import { RecurringType } from '../shared/recurring-type.enum';
 
 export interface IRecurringExpense extends Omit<IExpense, 'recurringExpense'> {
-  cron: string;
+  nextExecution: Date;
   recurringType: RecurringType;
   startDate?: Date;
   endDate?: Date;
