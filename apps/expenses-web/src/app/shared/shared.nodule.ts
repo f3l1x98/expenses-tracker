cde9c1fbcf4f9a96210b2de43fc53f9dd4cc2395
felix <felix.mueller0998@gmx.de>
2024-07-24 16:30:37 +0200

    Replace frontend nextExecution calc with backend one

--- apps/expenses-web/src/app/shared/shared.nodule.ts
+++ apps/expenses-web/src/app/shared/shared.nodule.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { FormatDatePipe } from './pipes/format-date.pipe';
 import { FormatCurrencyPipe } from './pipes/format-currency.pipe';
-import { NextCronExecutionPipe } from './pipes/next-cron-execution.pipe';
 import { CapitalizePipe } from './pipes/capitalize.pipe';
 import { HighlightNegativeCurrencyDirective } from './directives/highlight-negative-currency.directive';
 
@@ -10,14 +9,12 @@ import { HighlightNegativeCurrencyDirective } from './directives/highlight-negat
   exports: [
     FormatDatePipe,
     FormatCurrencyPipe,
-    NextCronExecutionPipe,
     CapitalizePipe,
     HighlightNegativeCurrencyDirective,
   ],
   declarations: [
     FormatDatePipe,
     FormatCurrencyPipe,
-    NextCronExecutionPipe,
     CapitalizePipe,
     HighlightNegativeCurrencyDirective,
   ],
