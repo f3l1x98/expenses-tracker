39dd2bb0904c8d1ffc85d730314fba3074e34ba2
felix <felix.mueller0998@gmx.de>
2025-06-19 16:13:45 +0200

    Adds tailwind and ran primeng primeflex-tailwind migration tool

--- apps/expenses-web/src/app/app.config.ts
+++ apps/expenses-web/src/app/app.config.ts
@@ -42,6 +42,12 @@ export const appConfig: ApplicationConfig = {
     providePrimeNG({
       theme: {
         preset: Aura,
+        options: {
+          cssLayer: {
+            name: 'primeng',
+            order: 'tailwind-base, primeng, tailwind-utilities',
+          },
+        },
       },
     }),
     { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
