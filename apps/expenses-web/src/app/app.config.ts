4d2112ac0dae2488c71851f700baeac4f992906f
felix <felix.mueller0998@gmx.de>
2025-06-19 14:58:39 +0200

    Removed ngrx/store and related deps/code

--- apps/expenses-web/src/app/app.config.ts
+++ apps/expenses-web/src/app/app.config.ts
@@ -11,10 +11,7 @@ import {
 } from '@angular/common/http';
 import { AuthInterceptor } from './core/auth/auth.interceptor';
 import { BrowserModule } from '@angular/platform-browser';
-import { StoreModule } from '@ngrx/store';
-import { EffectsModule } from '@ngrx/effects';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { isDevMode, importProvidersFrom } from '@angular/core';
+import { importProvidersFrom } from '@angular/core';
 import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -32,16 +29,6 @@ export const appConfig: ApplicationConfig = {
     importProvidersFrom(
       BrowserModule,
       BrowserAnimationsModule,
-      StoreModule.forRoot({}),
-      EffectsModule.forRoot([]),
-      StoreDevtoolsModule.instrument({
-        maxAge: 25, // Retains last 25 states
-        logOnly: !isDevMode(), // Restrict extension to log-only mode
-        autoPause: true, // Pauses recording actions and state changes when the extension window is not open
-        trace: false, //  If set to true, will include stack trace for every dispatched action, so you can see it in trace tab jumping directly to that part of code
-        traceLimit: 75, // maximum stack trace frames to be stored (in case trace option was provided as true)
-        connectInZone: true, // If set to true, the connection is established within the Angular zone
-      }),
       TranslateModule.forRoot({
         defaultLanguage: 'en',
         loader: {
